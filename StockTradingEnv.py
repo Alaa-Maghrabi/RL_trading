130c138c89425eeb9e4773dd839f1c515e8d5703
Alaa <alaa.maghrabi.m@gmail.com>
2021-06-25 00:11:56 +0200

    Fix Bug of Observability space (fix size)

--- StockTradingEnv.py
+++ StockTradingEnv.py
@@ -16,8 +16,9 @@ MAX_ACCOUNT_BALANCE = 2147483647
 INITIAL_ACCOUNT_BALANCE = 10000
 MAX_NUM_SHARES = 2147483647
 MAX_SHARE_PRICE = 5000
-MAX_OPEN_POSITIONS = 5
+MAX_OPEN_POSITIONS = 6
 MAX_STEPS = 20000
+MAX_TRADES = 1000
 
 class StockTradingEnv(gym.Env):
     """A stock trading environment for OpenAI gym"""
@@ -26,7 +27,7 @@ class StockTradingEnv(gym.Env):
     def __init__(self, df):
         super(StockTradingEnv, self).__init__()
         self.df = df
-        self.length_data = 10
+        self.length_data = 5
         self.reward_range = (0, MAX_ACCOUNT_BALANCE)
         self.lot = 0.1
         self.gain = 10
@@ -36,12 +37,21 @@ class StockTradingEnv(gym.Env):
         self.Open_trade = []
         self.number_open_trades = 0
 
+        # Total gain
+        self.gains = 0
+
+        # Counter for positive and negative trades
+        self.Pos_counter = 0
+        self.Neg_counter = 0
+        self.Closed_counter = 0
+        self.Open_counter = 0
+
         # Actions of the format Buy curr, Sell curr, Hold, etc.
         self.action_space = spaces.Box(
-            low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
+            low=np.array([0, 0]), high=np.array([4, 1]), dtype=np.float16)
         # Prices contains the OHCL values for the last five prices
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=(6, 6), dtype=np.float16)
+            low=0, high=1, shape=(self.length_data + 1 + MAX_OPEN_POSITIONS, 6), dtype=np.float16)
 
     def reset(self):
         # Reset the state of the environment to an initial state
@@ -49,7 +59,6 @@ class StockTradingEnv(gym.Env):
         self.net_worth = INITIAL_ACCOUNT_BALANCE
         self.max_net_worth = INITIAL_ACCOUNT_BALANCE
         self.pos_opened = []
-        self.number_open_pos = 0
         self.cost_basis = 0
         self.positions_closed = 0
         self.total_sales_value = 0
@@ -73,21 +82,37 @@ class StockTradingEnv(gym.Env):
                                            self.length_data, 'Volume'].values / MAX_NUM_SHARES,
         ])
 
+        # Transform list of dict to list of lists
+        trades_open = self.transform_dict_to_list(self.Open_trade)
+
+        trades_open += [['0','0','0','0','0','0']] * (MAX_OPEN_POSITIONS - len(trades_open))
+
+        # Change the shape of observation space to take into account for more open trades
+        # self.observation_space.shape = (self.length_data+1 + len(self.Open_trade),6)
+
         # Append additional data and scale each value to between 0-1
-        obs = np.append(frame, [[
+        obs = np.append(frame, np.append([[
             self.balance / MAX_ACCOUNT_BALANCE,
             self.max_net_worth / MAX_ACCOUNT_BALANCE,
-            self.shares_held / MAX_NUM_SHARES,
-            self.cost_basis,
-            self.total_shares_sold / MAX_NUM_SHARES,
-            self.total_sales_value / MAX_NUM_SHARES,
-        ]], axis=0)
+            self.number_open_trades / MAX_TRADES,
+            self.Closed_counter / MAX_TRADES,
+            self.Pos_counter / (self.Closed_counter+1),
+            self.gains / MAX_TRADES]], trades_open, axis = 0), axis=0)
 
         return obs
 
+    def transform_dict_to_list(self, dict):
+        full_list = []
+        for val in dict:
+            result = []
+            for key, value in val.items():
+                result.append(value)
+            full_list.append(result)
+        return full_list
+
     def closing_position(self):
         i = 0
-        while i < self.number_open_trades:
+        while i < self.Open_counter:
             if self.Open_trade[i]['position'] == 1:
                 # Check first if the min is below the SL just to be sure that during the movement of the candle the SL was
                 # not touched
@@ -101,7 +126,6 @@ class StockTradingEnv(gym.Env):
                     else:
                         self.Neg_counter += 1
                     del self.Open_trade[i]
-                    self.number_open_trades -= 1
                     self.Open_counter -= 1
                     self.Closed_counter += 1
 
@@ -115,7 +139,6 @@ class StockTradingEnv(gym.Env):
                     else:
                         self.Neg_counter += 1
                     del self.Open_trade[i]
-                    self.number_open_trades -= 1
                     self.Open_counter -= 1
                     self.Closed_counter += 1
 
@@ -129,7 +152,6 @@ class StockTradingEnv(gym.Env):
                     else:
                         self.Neg_counter += 1
                     del self.Open_trade[i]
-                    self.number_open_trades -= 1
                     self.Open_counter -= 1
                     self.Closed_counter += 1
 
@@ -144,7 +166,6 @@ class StockTradingEnv(gym.Env):
                     else:
                         self.Neg_counter += 1
                     del self.Open_trade[i]
-                    self.number_open_trades -= 1
                     self.Open_counter -= 1
                     self.Closed_counter += 1
 
@@ -158,7 +179,6 @@ class StockTradingEnv(gym.Env):
                     else:
                         self.Neg_counter += 1
                     del self.Open_trade[i]
-                    self.number_open_trades -= 1
                     self.Open_counter -= 1
                     self.Closed_counter += 1
 
@@ -172,7 +192,6 @@ class StockTradingEnv(gym.Env):
                     else:
                         self.Neg_counter += 1
                     del self.Open_trade[i]
-                    self.number_open_trades -= 1
                     self.Open_counter -= 1
                     self.Closed_counter += 1
             i += 1
@@ -186,9 +205,12 @@ class StockTradingEnv(gym.Env):
         if self.current_step > len(self.df.loc[:, 'Open'].values) - 6:
             self.current_step = 0
 
+        # Make sure to close the position that hit either the TP or the SL automatically
+        self.closing_position()
+
         delay_modifier = (self.current_step / MAX_STEPS)
 
-        reward = self.balance * delay_modifier
+        reward = self.gains * delay_modifier
         done = self.net_worth <= 0
 
         obs = self._next_observation()
@@ -201,53 +223,51 @@ class StockTradingEnv(gym.Env):
                                        self.df.loc[self.current_step, "Close"])
 
         action_type = action[0]
-        amount = action[1]
+        index = action[1]
 
-        if action_type < 1:
-            trade = {'entry': current_price, 'position': action_type, 'lot': self.lot,
-                     'SL': current_price - self.loss / 10000,
-                     'TP': current_price + self.gain / 10000, 'index': self.number_open_pos}
-
-            self.number_open_trades += 1
-            self.Open_counter += 1
-
-            self.pos_opened.append(trade)
-
-        elif action_type < 2:
-            trade = {'entry': current_price, 'position': action_type, 'lot': self.lot,
-                     'SL': current_price + self.loss / 10000,
-                     'TP': current_price - self.gain / 10000, 'index': self.number_open_pos}
-            self.number_open_trades += 1
-            self.Open_counter += 1
-            self.pos_opened.append(trade)
-
-        elif action_type < 3:
-            i = 0
-            while i < self.number_open_trades:
-                self.gains += (current_price - self.Open_trade[i]['entry']) * 10000
-                if (current_price - self.Open_trade[i]['entry']) > 0:
+        # 1st action:  Close a position
+        if 1 < action_type < 2:
+            if self.Open_counter>0 and int(index)<self.Open_counter:
+                self.gains += (current_price - self.Open_trade[int(index)]['entry']) * 10000
+                if (current_price - self.Open_trade[int(index)]['entry']) > 0:
                     self.Pos_counter += 1
                 else:
                     self.Neg_counter += 1
-                del self.Open_trade[i]
-                self.number_open_trades -= 1
+                del self.Open_trade[int(index)]
                 self.Open_counter -= 1
                 self.Closed_counter += 1
 
-        self.netWorth = self.balance + self.shares_held * current_price
+        # 2nd action: Open a sell entry
+        elif action_type < 3:
+            if self.Open_counter < MAX_OPEN_POSITIONS:
+                trade = {'entry': current_price, 'position': action_type, 'lot': self.lot,
+                         'SL': current_price + self.loss / 10000,
+                         'TP': current_price - self.gain / 10000, 'index': self.Open_counter}
+                self.Open_counter += 1
+                self.Open_trade.append(trade)
+
+        # 3rd action: Open a buy entry
+        elif action_type < 4:
+            if self.Open_counter < MAX_OPEN_POSITIONS:
+                trade = {'entry': current_price, 'position': action_type, 'lot': self.lot,
+                         'SL': current_price - self.loss / 10000,
+                         'TP': current_price + self.gain / 10000, 'index': self.Open_counter}
+
+                self.Open_counter += 1
+                self.Open_trade.append(trade)
+
+        # In the scenario I'm able to make a size variable observation space
+        self.action_space.high = np.array([4, self.Open_counter])
 
         if self.net_worth > self.max_net_worth:
             self.max_net_worth = self.net_worth
 
-        if self.shares_held == 0:
-            self.cost_basis = 0
 
     def render(self, mode='human', close=False):
         # Render the environment to the screen
         profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
         print(f'Step: {self.current_step}')
         print(f'Balance: {self.balance}')
-        print(f'Shares held: {self.shares_held} (Total sold: {self.total_shares_sold})')
-        print(f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
+        print(f'Shares held: {self.Open_counter} (Total sold: {self.gains})')
         print(f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
         print(f'Profit: {profit}')
